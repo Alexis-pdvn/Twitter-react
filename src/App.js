0c2b9ff4d04ba23cbed213c7e48329eed04964f5
Alexis-pdvn <alexispodevin1@gmail.com>
2022-05-26 20:01:47 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Affichage from './react/affichage/Affichage';
+import Saisie from './react/saisie/Saisie';
+
+const App = () => {
+  return (
+    <>
+       <Saisie />
+       <Affichage />
+    </>
+  );
+}
+
+export default App;
